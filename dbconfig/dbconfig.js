0dd0ebbbd1d227efecb995a47c9f4a54ac1ccfa0
elearninfotech1 <91933957+elearninfotech1@users.noreply.github.com>
2025-04-03 08:56:27 +0530

    Update dbconfig.js

--- dbconfig/dbconfig.js
+++ dbconfig/dbconfig.js
@@ -1,4 +1,2 @@
 let mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://elearninfotech1:Ua3fsh5z6uz6hHp5@mern9ammarch.0lddj.mongodb.net/?retryWrites=true&w=majority&appName=MERN9AMMarch"
-);
+mongoose.connect("mongodb+srv://elearninfotech1:47FY2pjSxDy5HDqn@samplepro.hkukr1v.mongodb.net/?retryWrites=true&w=majority&appName=samplepro");
